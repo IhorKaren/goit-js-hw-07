c2118efe80016db65481c88cf02e0235cc819628
Ihor Karen <karenihor@gmail.com>
2023-02-12 12:38:14 +0200

    second_gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryImgMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+// galleryContainer.addEventListener('click', onGalleryModalClick); !!!
+
+function createGalleryImgMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<div class='gallery__item'><a class='gallery__link' href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" alt="${description}" width="340"></a></div>`;
+    })
+    .join('');
+}
+
+// function onGalleryModalClick(e) {
+//   e.preventDefault();
+
+//   if (!e.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+// } !!!
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 300,
+});
