677bdfd6ece5d7851034ce8a094c8f4c0fdc1c18
Ihor Karen <karenihor@gmail.com>
2023-02-12 12:12:14 +0200

    first_gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -14,16 +14,42 @@ function createGalleryImgMarkup(gallery) {
     .map(({ preview, original, description }) => {
       return `<div class='gallery__item'><a class='gallery__link' href="${original}">
       <img class="gallery__image" 
-      src="${preview}" data-source="${original}" alt="${description}"></a></div>`;
+      src="${preview}" data-source="${original}" alt="${description}" width="340"></a></div>`;
     })
     .join('');
 }
 
-
 function onGalleryModalClick(e) {
-    e.preventDefault()
-    
-    if (!e.target.classList.contains('.gallery__link')) {
-      return;
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  onLightBoxModal(e);
+}
+
+function onLightBoxModal(e) {
+  const instance = basicLightbox.create(
+    `
+		<img src="${e.target.dataset.source}" width="1280" height="auto"/>
+        `,
+    {
+      onShow: instance => {
+        window.addEventListener('keydown', onEscKeyPress);
+      },
+      onClose: instance => {
+        window.removeEventListener('keydown', onEscKeyPress);
+      },
+    },
+  );
+  instance.show();
+
+  function onEscKeyPress(e) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = e.code === ESC_KEY_CODE;
+    if (isEscKey) {
+      instance.close();
     }
   }
+}
